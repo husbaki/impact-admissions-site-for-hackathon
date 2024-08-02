978d8dc33c612df4eab5032008f44007c224a79a
Bakyt_Aslan <165052961+Megasuperchel@users.noreply.github.com>
2024-08-02 21:30:51 +0500

    Add files via upload

---
+++ lib/module/home/inc-home-carousel.php
@@ -0,0 +1,93 @@
+<div class="container fill">
+	<div id="myCarousel" class="carousel slide">
+		<div class="carousel-inner">
+			<!-- Carousel Slide -->
+			<div class="item active">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-1'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/1.jpg') no-repeat center; background-size: cover;">
+								<div class="video-img-overlay">
+								</div>
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-4">
+								<div class='video-emp-block'>
+								</div>
+								<h2>RPhMS man?</h2>
+								<h3>how he got to Cambridge.</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Carousel Slide -->
+			<div class="item">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-2'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/2.jpg') no-repeat center; background-size: cover;">
+								<div class="video-img-overlay">
+								</div>
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-5">
+								<div class='video-emp-block'>
+								</div>
+								<h2>interview</h2>
+								<h3>interview with someone.</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Carousel Slide -->
+			<div class="item">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-3'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/3.jpg') no-repeat center; background-size: cover;">
+								<div class="video-img-overlay">
+								</div>
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-6">
+								<div class='video-emp-block'>
+								</div>
+								<h2>how to pass exams</h2>
+								<h3>8.5 ielts          1520 sat.</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ lib/module/inc-footer.php
@@ -0,0 +1,103 @@
+<footer>
+<div class="footer-con-container">
+	<div class="footer-con-overlay darkmode-header">
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class="footer-con-header">
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<table class="footer-table-out">
+					<tr>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Main</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="/blog">
+										<span>Blog</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="/about">
+										<span>About</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>GitHub</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://github.com/Megasuperchel" target="_blank">
+										<span>Repository</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Social</span>
+									</div>
+									<div class="footer-tx2-bound toggle-discord">
+										<span>Discord</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://www.youtube.com/@impactadmissions3532" target="_blank">
+										<span>YouTube</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+						
+					</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class='footer-con-credits'>
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class='footer-con-seprator'>
+					</div>
+					<div class="footer-con-foot">
+						<div class="footer-con-side">
+							<div class="footer-tx1-founder">
+								<a href="/about">
+								<div class="footer-tx1-developer pulsate">
+									<span>Founded by Bakyt Aslan, Erzhanuly Zhangir, Kurganbaev Zhangir</span>
+								</div>
+								</a>
+							</div>
+						</div>
+						<div class="footer-con-side">
+							<div class="footer-tx1-webmaster">
+								<a href="https://daginatsuko.com" target="_blank">
+								<div class="footer-tx1-developer pulsate">
+									<span>Website by Bakyt Aslan, Erzhanuly Zhangir, Kurganbaev Zhangir © 2024</span>
+								</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</footer>
